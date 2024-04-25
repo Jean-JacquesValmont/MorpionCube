e3c2418027578ee45fc280b62b1eb05af4c4d202
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2024-04-25 17:28:20 +0200

    Morpion game is functionnal in local

--- components/Square.tsx
+++ components/Square.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
-const Square = ({ onClick, value }) => {
+type SquareProps = {
+    // key: string | number
+    onClick: any
+    value: any
+}
+
+const Square = ({ onClick, value } : SquareProps) => {
   return (
     <button className="w-12 h-12 border-2 border-black" onClick={onClick}>
         {value}
